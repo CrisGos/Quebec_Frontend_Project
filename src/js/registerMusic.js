e85981d4741ae246a8f22f8ddf6cf5f98ff3ea83
Daws <castrogiraldosantiago@gmail.com>
2024-05-26 16:12:39 -0500

    fix: addition styles footer/header, and addition to the html

--- src/js/registerMusic.js
+++ src/js/registerMusic.js
@@ -4,34 +4,4 @@ import '../scss/registerMusic.scss'
 // Import all of Bootstrap's JS
 import * as bootstrap from 'bootstrap'
 
-//sections
-const form=document.querySelector('form')
-const artform=form.querySelectorAll('section')
-const sect0= form.querySelector('#band-info')
-const sect1= form.querySelector('#band-description')
-const sect2= form.querySelector('#accout-info')
-const btnSec=form.querySelector('.nxt-bck-btns')
-console.log(artform);
-let cont=0
-
-
-btnSec.addEventListener('click',(event)=>{
-    if (event.target.type=='submit') {
-        cont++
-        artform[cont].classList.replace('d-none','visible')
-        artform[--cont].classList.replace('visible','d-none')
-        
-        
-        
-        
-    }
-    if (event.target.type=='button') {
-        artform[cont].classList.replace('visible','d-none')
-        
-        cont--
-        artform[cont].classList.replace('d-none','visible')
-        
-    }
-
-})
 
