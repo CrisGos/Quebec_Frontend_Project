4e4c445f2050caeebbdc6b241d2a6c98f515bd85
Daws <castrogiraldosantiago@gmail.com>
2024-05-26 20:17:50 -0500

    fix: movement betwen sections solved

--- src/js/registerMusic.js
+++ src/js/registerMusic.js
@@ -3,39 +3,40 @@ import '../scss/registerMusic.scss'
 
 // Import all of Bootstrap's JS
 import * as bootstrap from 'bootstrap'
-let formRegister=document.querySelector("form")
+let formRegister = document.querySelector("form")
 let secCounter=1
-let seCurrent= formRegister.querySelector(`#section${secCounter}`)
-let divNxtBck=seCurrent.querySelector('.nxt-bck-btn')
-let nxtBtn=divNxtBck.querySelector('.btn-primary')
-let bckBtn=divNxtBck.querySelector(".btn-secondary")
-function refresh(section) {
-    divNxtBck=section.querySelector('.nxt-bck-btn')
+let seCurrent = formRegister.querySelector(`#section${secCounter}`)
+let divNxtBck = seCurrent.querySelector('.nxt-bck-btn')
+let nxtBtn = divNxtBck.querySelector('.btn-primary')
+let bckBtn = divNxtBck.querySelector(".btn-secondary")
+let arrsection=formRegister.querySelectorAll('section')
+arrsection.forEach(section=> {
+    divNxtBck = section.querySelector('.nxt-bck-btn')
     if (divNxtBck.contains(divNxtBck.querySelector('.btn-primary'))) {
-        nxtBtn=divNxtBck.querySelector('.btn-primary')
+        nxtBtn = divNxtBck.querySelector('.btn-primary')
+
+        nxtBtn.addEventListener('click', () => {
+            nextSection(secCounter)
+            secCounter++
+            console.log(seCurrent)
+            console.log(secCounter , "avanza");
+    
+        })
     }
-
-    bckBtn=divNxtBck.querySelector(".btn-secondary")
+    bckBtn = divNxtBck.querySelector(".btn-secondary")
     
-    nxtBtn.addEventListener('click',()=>{
-        nextSection(secCounter)
-        secCounter++
+    bckBtn.addEventListener('click', () => {
+        prevSection(secCounter)
+        secCounter--
         console.log(seCurrent)
-        console.log(secCounter);
-    
+        console.log(secCounter,"retrocede");
 
-})
-bckBtn.addEventListener('click',()=>{
-    nextSection(nxtBtn.getAttribute('btn-sec'))
-    secCounter--
-    
-})
+    })
+});
 
-}
-refresh(seCurrent)
 
 export function nextSection(currentSection) {
-    currentSection=Number(currentSection)
+    currentSection = Number(currentSection)
     const section = document.querySelector(`#section${currentSection}`)
     if (validateSection(section)) {
         section.classList.add('slide-out-left')
@@ -43,35 +44,32 @@ export function nextSection(currentSection) {
             section.classList.add('hidden')
             section.classList.remove('slide-out-left')
             const nextSection = document.querySelector(`#section${currentSection + 1}`)
-            refresh(nextSection)
             nextSection.classList.remove('hidden')
             nextSection.classList.add('slide-in-right')
             nextSection.addEventListener('animationend', function () {
                 nextSection.classList.remove('slide-in-right')
             }, { once: true })
         }, { once: true })
-        
+
     }
-    
+
 }
 
 
 export function prevSection(currentSection) {
-    currentSection=Number(currentSection)
+    currentSection = Number(currentSection)
     const section = document.querySelector(`#section${currentSection}`)
     section.classList.add('slide-out-right')
     section.addEventListener('animationend', function () {
         section.classList.add('hidden')
         section.classList.remove('slide-out-right')
         const prevSection = document.querySelector(`#section${currentSection - 1}`)
-        refresh()
         prevSection.classList.remove('hidden')
         prevSection.classList.add('slide-in-left')
         prevSection.addEventListener('animationend', function () {
             prevSection.classList.remove('slide-in-left')
         }, { once: true })
     }, { once: true })
-    refresh
 
 }
 
@@ -94,11 +92,11 @@ document.querySelector('#multiStepForm').addEventListener('submit', function (ev
 
 
 //image validator and dys
-document.querySelector('#imageUpload').addEventListener('change', function(event) {
+document.querySelector('#imageUpload').addEventListener('change', function (event) {
     const file = event.target.files[0]
-    if (file && file.type === 'image/jpeg' ||file && file.type === 'image/png') {
+    if (file && file.type === 'image/jpeg' || file && file.type === 'image/png') {
         const reader = new FileReader()
-        reader.onload = function(e) {
+        reader.onload = function (e) {
             document.querySelector('#imagePreview').src = e.target.result
         }
         reader.readAsDataURL(file)
