10b7a1bef0398d40456f20df112f2030d0297d8e
CrisGos <cristianortega9509@gmail.com>
2024-05-28 16:50:56 -0500

    Modificacion base de datos y loggin terminado por users y por artists

--- src/js/login.js
+++ src/js/login.js
@@ -1,33 +1,49 @@
-//form
-const form = document.querySelector('form')
-const email = document.querySelector('#email')
-const password = document.querySelector('#password')
+// form
+const form = document.querySelector('form');
+const email = document.querySelector('#email');
+const password = document.querySelector('#password');
 
 form.addEventListener('submit', async (event) => {
-  
+    event.preventDefault();
 
-    event.preventDefault()
-    const user = await validateEmail(email)
-    if (user === false) {
-        alert("usuario aún no registrado")
+    const user = await validateUser(email);
+    const artist = await validateArtist(email);
+
+    if (user === false && artist === false) {
+        alert("Usuario aún no registrado");
     } else {
-        if (user.password === password.value) {
-            localStorage.setItem("userOnline",JSON.stringify(user))
-            window.location.href="/"
+        if (user && user.password === password.value) {
+            localStorage.setItem("userOnline", JSON.stringify(user));
+            window.location.href = "/";
+        } else if (artist && artist.logInfo.password === password.value) {
+            localStorage.setItem("userOnline", JSON.stringify(artist));
+            window.location.href = "/";
         } else {
-            alert("la contraseña es incorrecta")
+            alert("La contraseña es incorrecta");
         }
     }
-})
+});
 
-// Validates that the email does not exist
-async function validateEmail(email) {
-    const response = await fetch(`http://localhost:3000/users?email=${email.value}`)
-    const data = await response.json()
+// Validates that the email does not exist for users
+async function validateUser(email) {
+    const response = await fetch(`http://localhost:3000/users?email=${email.value}`);
+    const data = await response.json();
 
     if (data.length > 0) {
-        return data[0]
+        return data[0];
     } else {
-        return false
+        return false;
+    }
+}
+
+// Validates that the email does not exist for artists
+async function validateArtist(email) {
+    const response = await fetch(`http://localhost:3000/artists?logInfo.email=${email.value}`);
+    const data = await response.json();
+
+    if (data.length > 0) {
+        return data[0];
+    } else {
+        return false;
     }
 }
