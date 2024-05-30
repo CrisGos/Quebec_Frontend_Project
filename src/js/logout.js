9c9dd7ed2fdf4e0f6cbef4e669c6f7ba4ff74dda
CrisGos <cristianortega9509@gmail.com>
2024-05-29 22:02:46 -0500

    logout fixed

--- src/js/logout.js
+++ src/js/logout.js
@@ -1,31 +1,46 @@
 // Function to control header buttons
 function controlHeader() {
-    // Check for data in localStorage to determine if a user is online
-    const userOnline = localStorage.getItem("userOnline");
-    if (userOnline) {
-     // Hide login buttons and registration menu
-      document.getElementById("login").style.display = "none";
-      document.getElementById("dropdown").style.display = "none";
-      // document.getElementById("the-contractor").style.display = "none";
-      // document.getElementById("the-artist").style.display = "none";
-     // Show new drop-down menu with user name
-      const userData = JSON.parse(userOnline);
-      const username = userData.username;
-      const userDropdown = document.getElementById("user-dropdown");
-      userDropdown.style.display = "block";
-      const userDropdownLink = userDropdown.querySelector(".dropdown-toggle");
-      userDropdownLink.textContent = username;
-  
-   // Add event to logout
-      document.getElementById("logout").addEventListener("click", function() {
-      // Clear localStorage and reload the page to simulate a logoff
-        localStorage.removeItem("userOnline");
-        location.reload();
-        window.location.href = "/"
-      });
+  // Check for data in localStorage to determine if a user is online
+  const userOnline = localStorage.getItem("userOnline");
+  if (userOnline) {
+
+    // Validate the existence of elements with specific IDs and hide them if found
+    const loginElement = document.getElementById("login");
+    if (loginElement) {
+      loginElement.style.display = "none";
     }
+
+    const dropdownElement = document.getElementById("dropdown");
+    if (dropdownElement) {
+      dropdownElement.style.display = "none";
+    }
+
+    const contractorElement = document.getElementById("the-contractor");
+    if (contractorElement) {
+      contractorElement.style.display = "none";
+    }
+
+    const artistElement = document.getElementById("the-artist");
+    if (artistElement) {
+      artistElement.style.display = "none";
+    }
+
+    const userData = JSON.parse(userOnline);
+    const username = userData.username;
+    const userDropdown = document.getElementById("user-dropdown");
+    userDropdown.style.display = "block";
+    const userDropdownLink = userDropdown.querySelector(".dropdown-toggle");
+    userDropdownLink.textContent = username;
+
+    // Add event to logout
+    document.getElementById("logout").addEventListener("click", function () {
+      // Clear localStorage and reload the page to simulate a logoff
+      localStorage.removeItem("userOnline");
+      location.reload();
+      window.location.href = "/"
+    });
   }
-  
-  // Execute the function when loading the DOM
-  document.addEventListener("DOMContentLoaded", controlHeader);
-  
+}
+
+// Execute the function when loading the DOM
+document.addEventListener("DOMContentLoaded", controlHeader);
